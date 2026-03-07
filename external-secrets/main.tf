35a94d73cd75518ba07300aff80b58001b91bf98
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2026-03-07 06:06:11 -0800

    feat: allow passing dynamic additional helm values (#134)
    * feat: allow passing dynamic additional helm values
    
    * Merge branch 'main' into eso-improvements

--- external-secrets/main.tf
+++ external-secrets/main.tf
@@ -1,8 +1,19 @@
+locals {
+  helm_values = []
+}
+
 resource "helm_release" "external-secrets" {
   name             = "external-secrets"
   namespace        = var.kube_namespace
   repository       = "https://charts.external-secrets.io"
   chart            = "external-secrets"
-  version          = "2.0.1"
+  version          = var.chart_version
   create_namespace = var.create_namespace
+
+  set = [
+    for k, v in var.helm_set_values : {
+      name  = k
+      value = v
+    }
+  ]
 }
